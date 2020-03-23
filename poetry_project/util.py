196b0819ba29e01961b3fafd489be34c4fec128d
Julian Stier <mail@julian-stier.de>
2020-03-23 10:36:23 +0100

    Removed deprecated file-loader based on path

--- poetry_project/util.py
+++ poetry_project/util.py
@@ -1,22 +1,11 @@
 from importlib_resources import files
-from pathlib import Path
 import tomlkit
 
 
-def load_pyproject_toml(base_path, file_name='pyproject.toml'):
-    # Deprecated, as it uses a concrete base path and not a package
-    d = Path(base_path)
-
-    while d.parent != d:
-        d = d.parent
-        pyproject_path = Path(d, file_name)
-        if pyproject_path.exists():
-            with open(file=str(pyproject_path)) as handle:
-                return tomlkit.parse(string=handle.read())
-    return None
-
-
 def load_toml_from_package(package, project_file_name='pyproject.toml'):
+    if package is None:
+        raise ValueError('Expected package to load a <{file}>-file from, but got <{package}>'.format(file=project_file_name, package=str(package)))
+
     package_path = files(package)
 
     while package_path.parent != package_path:
@@ -25,7 +14,7 @@ def load_toml_from_package(package, project_file_name='pyproject.toml'):
         if possible_project_file_path.exists():
             return tomlkit.parse(possible_project_file_path.read_text())
 
-    return None
+    raise ValueError('Could not find <{file}>-file in package <{package}>'.format(file=project_file_name, package=str(package)))
 
 
 def get_poetry_attribute(attribute_name, package, pyproject_toml=None, project_file_name='pyproject.toml'):
